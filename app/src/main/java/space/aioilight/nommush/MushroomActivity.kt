98609f5caf4a8de423b8b739245c1d1b180adddc
AioiLight <info@aioilight.space>
2020-07-13 00:29:29 +0900

    AndroidManifestにintent-filterを追加

--- app/src/main/java/space/aioilight/nommush/MushroomActivity.kt
+++ app/src/main/java/space/aioilight/nommush/MushroomActivity.kt
@@ -1,9 +1,13 @@
 package space.aioilight.nommush
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MushroomActivity : AppCompatActivity() {
+
+    private val ACTION_INTERCEPT = "com.adamrocker.android.simeji.ACTION_INTERCEPT"
+    private val REPLACE_KEY = "replace_key"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mushroom)
