24bdca4a4eeba4b51a318adf837f347caed392be
AioiLight <info@aioilight.space>
2020-07-13 00:26:00 +0900

    MushroomActivity

---
+++ app/src/main/java/space/aioilight/nommush/MushroomActivity.kt
@@ -0,0 +1,11 @@
+package space.aioilight.nommush
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MushroomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mushroom)
+    }
+}
