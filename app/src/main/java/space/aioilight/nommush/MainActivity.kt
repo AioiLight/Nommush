52c2ad3def34107258b163a3cb50536405ed6e8b
AioiLight <info@aioilight.space>
2020-07-13 18:57:17 +0900

    Web サイトへのリンクと IME 選択ボタン

--- app/src/main/java/space/aioilight/nommush/MainActivity.kt
+++ app/src/main/java/space/aioilight/nommush/MainActivity.kt
@@ -1,11 +1,28 @@
 package space.aioilight.nommush
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_web).setOnClickListener {
+            val url = "https://racing-lagoon.info/nomu/translate.php"
+            val uri = Uri.parse(url)
+            val intents = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intents)
+        }
+
+        findViewById<Button>(R.id.button_ime).setOnClickListener {
+            val imeManager = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imeManager.showInputMethodPicker()
+        }
     }
 }
